426a4bd40e6e718c4b0ea1a3a47d9e7eebe1976b
Adam Z Abounnaaim <aabounna@purdue.edu>
2023-08-23 12:48:23 -0400

    Success again!

--- Console App - Lab 1/Console App - Lab 1.cpp
+++ Console App - Lab 1/Console App - Lab 1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World! My name is Adam and I like beef stroganoff.\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
